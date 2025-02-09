8e1f9a5d564eacaa1f3d0143f453f560f5194d03
Patrick H Madden <pmadden@acm.org>
2025-02-09 12:12:33 -0500

    Integrating KaHyPar, working on the framework

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let hg = HyperGraph::hm_sample();
     println!("Hypergraph details: {}", hg);
     let mp = Metapartitioner::new();
-    let (part, bins, cut) = mp.partition(&hg);
+    let (part, bins, cut) = mp.hg_partition(&hg);
     println!("Cut is {}", cut);
 
 }
--- src/metapartitioner.rs
+++ src/metapartitioner.rs
@@ -2,6 +2,7 @@
 use crate::hypergraph::HyperGraph;
 use std::os::raw::{c_int, c_uint, c_ulong};
 use std::fmt;
+use kahypar_r;
 
 pub enum Partitioner {
     H, // hMetis
@@ -21,6 +22,7 @@ pub struct Metapartitioner {
     pub k: usize,
     pub partitioner_type: Partitioner,
     pub objective: Objective,
+    pub seed: u64,
 }
 
 
@@ -36,6 +38,7 @@ impl Metapartitioner {
             k: 2,
             partitioner_type: Partitioner::D,
             objective: Objective::C,
+            seed: 8675309,
         }
     }
     /// Partitions the graph, using the partitioner and k values indicated.  Will
@@ -43,19 +46,46 @@ impl Metapartitioner {
     /// the bin assignment as the first vector, the total weight in each bin
     /// in the second, and the cost based on the selected objective as the third
     /// value.
-    pub fn partition(&self, hg: &HyperGraph) -> (Vec<c_int>,Vec<c_int>,usize) {
-        (Vec::new(), Vec::new(), 0)
+    pub fn hg_partition(&self, hg: &HyperGraph) -> (Vec<c_int>,Vec<c_int>,usize) {
+        match self.partitioner_type {
+            Partitioner::K => {return self.hg_ka_partition(hg);},
+            _ => {println!("Not supported"); return (Vec::new(),Vec::new(),0); }
+        }
     }
 
+    pub fn hg_ka_partition(&self, hg: &HyperGraph) -> (Vec<c_int>,Vec<c_int>,usize) {
+        unsafe {
+            let mut partition = hg.part.clone();
+            kahypar_r::partition(
+                hg.vtxwt.len() as u32,
+                (hg.eind.len() - 1) as u32,
+                hg.hewt.as_ptr(),
+                hg.vtxwt.as_ptr(),
+                hg.eind.as_ptr(),
+                hg.eptr.as_ptr(),
+                partition.as_mut_ptr(),
+                self.k as i32,
+                self.num_starts as i32, // Passes
+                1 as u64 // Seed
+            );
+            // for i in 0..cells.len() {
+            //     if hg.part[i] == 0 {
+            //         nmod1.push(cells[i]);
+            //     } else {
+            //         nmod2.push(cells[i]);
+            //     }
+            // }
+        }
+        (Vec::new(), Vec::new(), 0)      
+    }
+    
+    
+    
     // The dumb partitioner.  Sorts the vertices by weight (descending), then assigns
     // the vertices to bins to minimize the difference in weights
-    
-
     pub fn partition_dumb(&self, hg: &HyperGraph) -> Vec<c_int> {
-        let mut bins = Vec::new();
-        for _i in 0..self.k {
-            bins.push(0 as c_int);
-        }
+        let mut bins = vec![0 as c_int; self.k];
+
         let mut verts = Vec::with_capacity(hg.vtxwt.len());
         for i in 0..hg.vtxwt.len() {
             verts.push(VW{
@@ -72,8 +102,42 @@ impl Metapartitioner {
         }
 
         part
+    }
 
-        
+    pub fn show(&self, hg: &HyperGraph, part: &Vec<c_int>, bins: &Vec<c_int>, cut: usize) {
+        println!("Graph: {} vertices, {} edges.  Cut {}", hg.vtxwt.len(), hg.eind.len() - 1, cut);
+        for b in bins {
+            println!("Bin weight: {}", b);
+        }
+        let mut max_v = part.len();
+        if max_v > 10 { max_v = 10;}
+        for i in 0..max_v {
+            println!("Vertex {} mapped to bin {}", i, part[i]);
+        }
+    }
+
+    pub fn evaluate(&self, hg: &HyperGraph, part: &Vec<c_int>) -> (Vec<c_int>,usize) {
+        let mut bins = vec![0 as c_int; self.k];
+        for i in 0..part.len() {
+            bins[part[i] as usize] = bins[part[i] as usize] + hg.vtxwt[i];
+        }
+
+        let mut cut = 0;
+        for i in 1..hg.eptr.len() {
+            let mut netbin = vec![false; self.k];
+            let mut count = 0;
+            for v in hg.eind[i - 1]..hg.eind[i] {
+                if !netbin[part[v as usize] as usize] {
+                    count = count + 1;
+                    netbin[part[v as usize] as usize] = true;
+                }
+            }
+            if count > 1 {
+                cut = cut + 1;
+            }
+        }
+
+        (bins, cut)
     }
 
 }
